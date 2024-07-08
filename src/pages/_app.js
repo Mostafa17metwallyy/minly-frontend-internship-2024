0da55196679a0ef8532ca0f09e1947fc818c459a
Bassam Zakaria <bassam.s.zakaria@gmail.com>
2024-07-08 09:49:21 +0300

    initial material ui commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
+import * as React from 'react';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../styles/theme';
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  
+  return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
