029436d95e8c668eda6508b90f2d9519d1a4b2f5
Mostafa17metwallyy <120329212+Mostafa17metwallyy@users.noreply.github.com>
2024-07-31 13:18:43 +0300

    made sign in and sign up pages

---
+++ src/pages/auth/signIn/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword,getAuth } from 'firebase/auth';
+import "@/firebase"
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn =(e)=>{
+    e.preventDefault(); 
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email,password)
+    .then((userCredential) => {
+        console.log(userCredential);
+    }).catch((error)=>{
+        console.log(error)
+    })
+  }
+
+  return (
+    <div >
+      <form onSubmit={handleSignIn}>
+        <h1>Log In</h1>
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Enter your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button type='submit'>Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/auth/signUp/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword,getAuth } from 'firebase/auth';
+import "@/firebase"
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp =(e)=>{
+    e.preventDefault(); 
+    console.log(email,password)
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email,password)
+    .then((userCredential) => {
+        console.log(userCredential);
+    }).catch((error)=>{
+        console.log(error)
+    })
+  }
+
+  return (
+    <div >
+      <form onSubmit={handleSignUp}>
+        <h1>Sign Up</h1>
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Enter your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button type='submit'>Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
