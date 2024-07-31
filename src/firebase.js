8fa21670ff4f572b71c27c910f586706298dae29
Mostafa17metwallyy <120329212+Mostafa17metwallyy@users.noreply.github.com>
2024-07-31 13:19:05 +0300

    used firebase for the authentication

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy2Ur2O7ig-R6BSoRoLeLNIG6XIR-gl9g",
+  authDomain: "mmdb-73da7.firebaseapp.com",
+  projectId: "mmdb-73da7",
+  storageBucket: "mmdb-73da7.appspot.com",
+  messagingSenderId: "562902419217",
+  appId: "1:562902419217:web:2513650d618c965fc5eeb0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
